2d795dca1b484b7068a4e1a2fbc10ef9c9b07cdc
GARVJAIN343 <garvjain343@gmail.com>
2022-05-16 01:04:30 +0530

    Update

--- abc/CustomerData.js
+++ abc/CustomerData.js
@@ -2,81 +2,63 @@ import React, { useEffect, useState } from 'react';
 import { MdDelete } from "react-icons/md";
 import Data from './Data.js'; 
 import DropDown from './DropDown.js';
-import Chip from '@mui/material/Chip';
-
 
 const CustomerData = () => {
+
+  const [data, setData] = useState(Data);
   const [value, setValue] = useState("");
   const [dropDown, setDropDown] = useState([]);
   const [list, setList] = useState([]);
   const [finalList, setFinalList] = useState([]);
 
   useEffect(()=>{
-    let filterData = Data.filter((element)=>{
+    let filterData = data.filter((element)=>{
+      // console.log(value);
       if (element.name.toLowerCase().includes(value.toLowerCase()))
       return true;
       if (element.email.includes(value)) return true;
       return false;
     })
-    let trump = value.trim();
-    if(trump === "" || trump === " "){
-      setDropDown([]);
-    }
-    else{
+    if(value != ""){
       setDropDown(filterData);
     }
   },[value])
 
   const changeHandler = (e) =>{
     e.preventDefault();
+    // console.log(value);
     setValue(e.target.value);
   }
 
-  const addItem = (e) => { 
-    e.preventDefault();
+  const addItem = () => { 
     let answer = list.map((event) => {
-      console.log(event);
       let propertyValues = Object.values(event);
-      return (propertyValues);
+      // console.log(propertyValues);
+      setFinalList([...finalList, propertyValues]);
+      return (event);
     })
 
-     setFinalList(answer);
+    // console.log(answer);
+    // setFinalList([...finalList, list]);
     setDropDown([]); 
     setValue("");
     setList([]);
   }
   
   const handleDelete = (id) => {
-    const filteredUsersList = finalList.filter((item,index) => index !== id);
+    let inn = id;
+    console.log(inn);
+    const filteredUsersList = finalList.filter((item) => item[0] !== inn);
     setFinalList(filteredUsersList);
 };  
-
-const handleDelete1 = (chipToDelete) => {
-    console.log(chipToDelete);
-   let chipDel = list.filter((chip) => {
-     console.log(chip);
-    return(chip.id !== chipToDelete.id)
-  });
-  setList(chipDel);
-};
 
   return (
     
     <div className='card'>
-   
               <div className='cardHeading'><h3>Customer Success Managers</h3></div>
               <div className='field'>
-              <div className='chips'>
-              {list.map((item, index) => ( 
-              <Chip
-              style={{backgroundColor:'#d7dcff', color:'#2235a3'}}
-              key={index}
-              label={item.name}
-              onDelete={()=>{handleDelete1(item)}}/>
-              ))}
-              <input style={{border:0 , outline:0}} id='garv' type='text' placeholder="Add by Name or email" value={value} onChange={changeHandler}>
+              <input id='garv' type='text' placeholder="Add by Name or email" value={value} onChange={changeHandler}>
               </input> 
-              </div>
               <DropDown 
               setList={setList}
               list={list}
@@ -85,23 +67,25 @@ const handleDelete1 = (chipToDelete) => {
               </div>
               <div className='allCustomer'>
            {
-              finalList.map((curEle,index)=>{
+              finalList.map((curEle)=>{
+              // console.log(curEle);
               let avtar = curEle[1];
               let nameArray = avtar.split(" ");
               let firstChar = nameArray[0][0].toUpperCase();
               let lastChar = nameArray[nameArray.length - 1][0].toUpperCase();
               let str = firstChar+lastChar;
+              let id = curEle[0];
               
                   return (
               
-              <div className='customer' key={index}>
+              <div className='customer' key={curEle[0]}>
                 <div className='cardavtar'><h5>{str}</h5></div>
                 <div className='name'><h4>{curEle[1]}</h4>
                       <span>{curEle[3]}</span>
                 </div>
                 <div className='delete'>
 
-                  <button className='deletebtn' onClick={() =>{handleDelete(index);}}>
+                  <button className='deletebtn' onClick={() =>{handleDelete(curEle[0]);}}>
                   <MdDelete style={{width: 20, height:20, color:"#1e31a1"}}/>
                   </button>
                 </div>
